52e4f57108ef952adac66987f990a5f122b1fbe7
ilias <iliaschokairi@gmail.com>
2024-02-01 00:00:50 +0100

    push to open in the sandbox for python3

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""""
+
+from fabric.api import local
+import os
+from datetime import datetime
+
+def do_pack():
+    """"""
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = f"web_static_{timestamp}.tgz"
+        archive_path = f"versions/{archive_name}"
+        try:
+            local(f"tar -cfz {archive_path} web_static")
+            return archive_path
+        except:
+            return None
