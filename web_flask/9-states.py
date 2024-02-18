cce4405f8e46b24305f58f7a0820a7c40db5dc8e
ilias <iliaschokairi@gmail.com>
2024-02-18 17:50:57 +0100

    i think i found something

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -17,4 +17,18 @@ def states():
 @app.route('/states/<id>', strict_slashes=False)
 def cities_by_state():
     """display the states with there cities"""
-    
+    states_non_sorted = list(storage.all(State).values())
+    cities_non_sorted = list(storage.all(City).values())
+    states = sorted(states_non_sorted, key=lambda state: state.name)
+    cities = sorted(cities_non_sorted, key=lambda city: city.name)
+    return render_template("9-states.html", states=states, cities=cities)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """remove the session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)    
+
