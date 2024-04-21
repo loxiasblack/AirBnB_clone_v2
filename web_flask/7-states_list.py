338087b4f88252ea1408e37b6b7353ae80c1ddaa
ilias <iliaschokairi@gmail.com>
2024-04-21 16:55:43 +0100

    display list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""list of states"""
+"""display my database"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,10 +8,11 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def list_states():
+def states_list():
+    """display the list of state"""
     states = list(storage.all(State).values())
     sorted_list = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_list)
+    return render_template("7-states_list.html", states=sorted_list)
 
 
 @app.teardown_appcontext
@@ -21,4 +22,4 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
