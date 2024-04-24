5ea822c5ed906045ef087c5f7eebc4d7664cccd8
ilias <iliaschokairi@gmail.com>
2024-04-24 11:36:56 +0100

    create a flask script that display an html with states_list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""display my database"""
+"""just a power of web framwirk"""
+
 from flask import Flask, render_template
-from models import storage
 from models.state import State
+from models import storage
+
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """display the list of state"""
+    """return an html with lists"""
     states = list(storage.all(State).values())
-    sorted_list = sorted(states, key=lambda state: state.name)
-    return render_template("7-states_list.html", states=sorted_list)
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """remove the current storage"""
+def terdown_context(exception):
+    """remove the current session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
